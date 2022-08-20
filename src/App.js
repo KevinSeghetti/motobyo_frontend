731af5e769cc5381d38294a1d34c33ce9c7618e3
Kevin Seghetti <kts@tenetti.org>
2022-08-20 12:05:48 -0700

    added fetching and display of employee list from server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+   const [employees, setEmployees] = useState([]);
+   useEffect(() => {
+      fetch('http://localhost:3000/employees')
+         .then((response) => response.json())
+         .then((data) => {
+            console.log(data);
+            setEmployees(data);
+         })
+         .catch((err) => {
+            console.log(err.message);
+         });
+   }, []);
+
+   console.log("results",employees)
+
+   return (
+          <div className="employees-container">
+         {employees.map((employee) => {
+            return (
+               <div className="employee-card" key={employee.id}>
+                  <h2 className="employee-title">{employee.firstName}</h2>
+                  <p className="employee-body">{employee.middleInitial}</p>
+                  <p className="employee-body">{employee.lastName}</p>
+                  <div className="button">
+                  <div className="delete-btn">Delete</div>
+                  </div>
+               </div>
+            );
+         })}
+      </div>
+
+   );
+};
+
+
+
+
+function NotApp() {
   return (
     <div className="App">
       <header className="App-header">
