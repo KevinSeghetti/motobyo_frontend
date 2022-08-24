6e663bdd5539957912f61cf5c314447802ecac98
Kevin Seghetti <kts@tenetti.org>
2022-08-24 13:28:36 -0700

    improved look of employee list nav bar

--- src/components/EmployeeHeader.js
+++ src/components/EmployeeHeader.js
@@ -3,9 +3,10 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Dropdown from 'react-bootstrap/Dropdown'
 import DropdownButton from 'react-bootstrap/DropdownButton'
-import Navbar from 'react-bootstrap/Navbar'
-import { PersonPlus } from 'react-bootstrap-icons'
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container'
 
+import { PersonPlus } from 'react-bootstrap-icons'
 
 //-------------------------------------------------------------------------------
 
@@ -21,21 +22,24 @@ const EmployeeHeader = (props) => {
                <h1 className="header">Welcome To Employee editing</h1>
             </Col>
          </Row>
-         <Navbar>
-            <Row>
-               <Col>
-                  <DropdownButton id="dropdown-basic-button" title="Filter"
-                     onSelect = { (key) => props.updateShowStatus(key) }
+
+         <Navbar bg="light" >
+            <Container>
+               <Navbar.Text>
+                  <DropdownButton id="dropdown-basic-button" title={ "Filter: "+props.showStatus }
+                        onSelect = { (key) => props.updateShowStatus(key) }
                      >
                      <Dropdown.Item eventKey="active"  >Active</Dropdown.Item>
                      <Dropdown.Item eventKey="inactive">Inactive</Dropdown.Item>
                      <Dropdown.Item eventKey="all"     >All</Dropdown.Item>
                   </DropdownButton>
-               </Col>
-               <Col>
-                  <Button size='sm' onClick={props.newEmployee}><PersonPlus /> New</Button>
-               </Col>
-            </Row>
+               </Navbar.Text>
+               <Navbar.Collapse className="justify-content-end">
+                  <Navbar.Text>
+                     <Button size='sm' onClick={props.newEmployee}><PersonPlus /> New</Button>
+                  </Navbar.Text>
+               </Navbar.Collapse>
+            </Container>
          </Navbar>
       </div>
    )
