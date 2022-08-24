e677d0923c8e1405bc8377c2a29a1bee3e6c1b10
Kevin Seghetti <kts@tenetti.org>
2022-08-24 12:52:45 -0700

    converted employee list view to react table so it looks better

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,49 +1,53 @@
 import './Employee.css'
 
+import Table from 'react-bootstrap/Table'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 import { PersonDashFill,PersonLinesFill } from 'react-bootstrap-icons'
 
+
 const EmployeeList = (props) => {
 
    return (
-      <div className="employees-list">
-         <Row >
-           <Col>Id</Col>
-           <Col>First Name</Col>
-           <Col>Middle Initial</Col>
-           <Col>Last Name</Col>
-           <Col>Edit</Col>
-           <Col>Delete</Col>
-         </Row>
+      <Table striped bordered className="employees-list">
+         <thead>
+           <tr>
+             <th>Id</th>
+             <th>First Name</th>
+             <th>Middle Initial</th>
+             <th>Last Name</th>
+             <th>Edit</th>
+             <th>Delete</th>
+           </tr>
+         </thead>
+         <tbody>
+           {
+              props.employees.filter( employee =>
+                  ( props.showStatus === 'all') ||
+                  ( props.showStatus === 'active' && employee.status === true) ||
+                  ( props.showStatus === 'inactive' && employee.status === false)
+              )
+              .map((employee) => {
+                 return (
+                    <tr key={employee.id}>
+                      <td>{employee.id}</td>
+                      <td><Button variant="link" onClick={() => props.viewEmployee(employee)}>{employee.firstName}</Button></td>
+                      <td>{employee.middleInitial}</td>
+                      <td>{employee.lastName}</td>
+                      <td className="edit-btn">
+                         <Button className=" btn-edit" size="sm" onClick={() => props.editEmployee(employee)}><PersonLinesFill />&nbsp;Edit</Button>
+                      </td>
 
-
-         {
-            props.employees.filter( employee =>
-                ( props.showStatus === 'all') ||
-                ( props.showStatus === 'active' && employee.status === true) ||
-                ( props.showStatus === 'inactive' && employee.status === false)
-            )
-            .map((employee) => {
-               return (
-                  <Row key={employee.id}>
-                    <Col>{employee.id}</Col>
-                    <Col><Button variant="link" onClick={() => props.viewEmployee(employee)}>{employee.firstName}</Button></Col>
-                    <Col>{employee.middleInitial}</Col>
-                    <Col>{employee.lastName}</Col>
-                    <Col className="edit-btn">
-                       <Button className=" btn-edit" size="sm" onClick={() => props.editEmployee(employee)}><PersonLinesFill />&nbsp;Edit</Button>
-                    </Col>
-
-                    <Col className="delete-btn">
-                       <Button variant="danger" className=" btn-delete" size="sm" onClick={() => props.deleteEmployee(employee)}><PersonDashFill />&nbsp;Delete</Button>
-                    </Col>
-                  </Row>
-               )
-            })
-         }
-      </div>
+                      <td className="delete-btn">
+                         <Button variant="danger" className=" btn-delete" size="sm" onClick={() => props.deleteEmployee(employee)}><PersonDashFill />&nbsp;Delete</Button>
+                      </td>
+                    </tr>
+                 )
+              })
+           }
+        </tbody>
+      </Table>
 
    )
 }
